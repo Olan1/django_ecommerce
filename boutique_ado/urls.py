c7fde956ce9162749540a6b1e580caa5d1a99678
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-06-16 16:03:06 +0000

    Added home app and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    # Include urls.py from home app
+    path('', include('home.urls')),
 ]
