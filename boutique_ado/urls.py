72f98827bf38cb8397125f4fdb7c0bf0f31ec9f9
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-06-17 13:09:15 +0000

    Completed homepage header and css

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     # Include urls.py from home app
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # Adds media url to list of urls
