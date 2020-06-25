d7061e8f2b2ad7f83201938cdfb55d87824ef41d
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-06-25 12:40:59 +0000

    Customized admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,29 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    # Tuple which tells admin which fields to display
+    list_display = (
+            'sku',
+            'name',
+            'category',
+            'price',
+            'rating',
+            'image',
+        )
+        
+    # Sort products by field (The 'sku' field in this case)
+    ordering = ('sku',)
+    
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+            'friendly_name',
+            'name',
+        )
+    
+    
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
