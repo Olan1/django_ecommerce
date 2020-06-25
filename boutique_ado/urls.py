8da4b538a06d7d15eec7d79ac7fcdded11a90a73
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-06-25 12:54:11 +0000

    Added products views and template

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     # Include urls.py from home app
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # Adds media url to list of urls
