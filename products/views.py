a98725d4e345bc82066e6357fbde02cb145c905c
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-06-27 15:06:47 +0000

    Added search functionality

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
 from .models import Product
+# Used to generate search query (allows use of or functionality in DB filtering)
+from django.db.models import Q
 
 
 # Create your views here.
@@ -9,9 +12,36 @@ def all_products(request):
     """
     
     products = Product.objects.all()
+    query = None
+    
+    # If search form submits GET request
+    if request.GET:
+        # q is the name of search input
+        if 'q' in request.GET:
+            query = request.GET['q']
+            # If search input (q) is blank
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                # Redirect to products url
+                return redirect(reverse('products'))
+            
+            # Checks if object in DB contains search query in either the item name or description fields
+            # | is the or statement
+            #  The i before contains makes the query case insensitive
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
     
     context = {
         'products': products,
+        'search_term': query,
     }
     
     return render(request, 'products/products.html', context)
+    
+    
+def product_detail(request, product_id):
+    # del request.session['cart']
+    product = get_object_or_404(Product, pk=product_id)
+    context = {'product': product}
+
+    return render(request, 'products/product_detail.html', context)
