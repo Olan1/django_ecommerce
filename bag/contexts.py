4c9eaf9df0717b24d6514ad6dd111cce4ada0d86
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-07-02 18:03:55 +0000

    Added quantity input +/- buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -15,19 +15,35 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
     
     # Iterate through item_id's and their quantities in bag (from bag session variable)
-    for item_id, quantity in bag.items():
-        # Get product
-        product = get_object_or_404(Product, pk=item_id)
-        # Add cost of item times its quantity to the total
-        total += quantity * product.price
-        # Increment thr product count by the quantity of the current iteration item
-        product_count += quantity
-        # Dict of item id, quantity, and product object
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # If item_data is an integer, item_data is the quantity of the item
+        if isinstance(item_data, int):
+            # Get product
+            product = get_object_or_404(Product, pk=item_id)
+            # Add cost of item times its quantity to the total
+            total += item_data * product.price
+            # Increment thr product count by the quantity of the current iteration item
+            product_count += item_data
+            # Dict of item id, quantity, and product object
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # If item_data is not an int, it is a dictionary
+        else:
+            # Iterate through items inner dictionary (item_data)
+            for size, quantity in item_data['items_by_size'].items():
+                # Get product
+                product = get_object_or_404(Product, pk=item_id)
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
     
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -15,15 +15,35 @@ def add_to_bag(request, item_id):
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     
+    # Get product size from POST request
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    
     # Get the bag session variable if it exists, or create it if not
     bag = request.session.get('bag', {})
     
-    # If the item id key already exists within the bag dict, increment its value
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    # If item id key does not exist within dict, create it and add quantity as value
+    # If a product with sizes is being added to the bag
+    if size:
+        # If item already in the bag
+        if item_id in list(bag.keys()):
+            # If item already exists in bag of the same size, increment quantity
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            # If item in bag but different size, add item with new size
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        # If item not already in bag, add it as dictionary with sizes
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    # If product without sizes is being added to bag
     else:
-        bag[item_id] = quantity
+        # If item already in bag, increment quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        # If item not already in bag, create it with value = quantity
+        else:
+            bag[item_id] = quantity
     
     # Overwrite the bag session variable with the updated version
     request.session['bag'] = bag
