33fa5c7ee24cc9746cdf7dd40cc54f931ea19ab9
Ubuntu <ubuntu@ip-172-31-61-210.ec2.internal>
2020-07-14 16:56:02 +0000

    Added checkout form data caching in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -76,35 +76,74 @@ form.addEventListener('submit', function(ev) {
   $('#payment-form').fadeToggle(100);
   $('#loading-overlay').fadeToggle(100);
   
-  // Send card info securely to Stripe
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-      card: card,
-    }
-  // Then execute this function on result
-  }).then(function(result) {
-    if (result.error) {
-      // Get error div element
-      var errorDiv = document.getElementById('card-errors');
-      // Show error to your customer (e.g., insufficient funds)
-      var html = `
-        <span class="icon" role="alert">
-          <i class="fas fa-times"></i>
-        </span>
-        <span>${ result.error.message }</span>`;
-      $(errorDiv).html(html);
-      
-      $('#payment-form').fadeToggle(100);
-      $('#loading-overlay').fadeToggle(100);
-      
-      // Reenable card element and submit button to allow user to fix error and resubmit
-      card.update({'disabled': false});
-      $('#submit-button').attr('disabled', false);
-    } else {
-      // If the payment has been processed successfully, submit the form
-      if (result.paymentIntent.status === 'succeeded') {
-        form.submit();
+  
+  var saveInfo = Boolean($('#id-save-info').attr('checked'));
+  var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+  var postData = {
+    'csrfmiddlewaretoken': csrfToken,
+    'client_secret': clientSecret,
+    'save_info': saveInfo
+  };
+  var url = '/checkout/cache_checkout_data/';
+  
+  $.post(url, postData).done(function() {
+    // Send card info securely to Stripe
+    stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: $.trim(form.full_name.value),
+          phone: $.trim(form.phone_number.value),
+          email: $.trim(form.email.value),
+          address: {
+            line1: $.trim(form.street_address1.value),
+            line2: $.trim(form.street_address2.value),
+            city: $.trim(form.town_or_city.value),
+            country: $.trim(form.country.value),
+            state: $.trim(form.county.value),
+          }
+        }
+      },
+      shipping: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        address: {
+          line1: $.trim(form.street_address1.value),
+          line2: $.trim(form.street_address2.value),
+          city: $.trim(form.town_or_city.value),
+          country: $.trim(form.country.value),
+          postal_code: $.trim(form.postcode.value),
+          state: $.trim(form.county.value),
+        }
       }
-    }
-  });
+    // Then execute this function on result
+    }).then(function(result) {
+      if (result.error) {
+        // Get error div element
+        var errorDiv = document.getElementById('card-errors');
+        // Show error to your customer (e.g., insufficient funds)
+        var html = `
+          <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+          </span>
+          <span>${ result.error.message }</span>`;
+        $(errorDiv).html(html);
+        
+        $('#payment-form').fadeToggle(100);
+        $('#loading-overlay').fadeToggle(100);
+        
+        // Reenable card element and submit button to allow user to fix error and resubmit
+        card.update({'disabled': false});
+        $('#submit-button').attr('disabled', false);
+      } else {
+        // If the payment has been processed successfully, submit the form
+        if (result.paymentIntent.status === 'succeeded') {
+          form.submit();
+        }
+      }
+    });
+  }).fail(function () {
+    // Just reload page, error will be in django messagess
+    location.reload();
+  })
 });
